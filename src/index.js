a3450f178ef68da5d222022cfb65c095871ef8b5
mukeshkumarmaurya67 <mukeshkumarmaurya70@gmail.com>
2023-12-12 23:21:56 +0530

    testing

--- src/index.js
+++ src/index.js
@@ -11,7 +11,6 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
  
     <App />
- 
 );
 
 // If you want to start measuring performance in your app, pass a function
