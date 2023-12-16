f3f6439ec6483705f745d0ea59d7751566bde153
mukeshkumarmaurya67 <mukeshkumarmaurya70@gmail.com>
2023-12-16 18:47:21 +0530

    conflict resolved

--- src/App.jsx
+++ src/App.jsx
@@ -13,10 +13,6 @@ const App = () => {
 
     return (
         <>
-     <p>dbvvghvhud</p>
-     <p>dbvvghvhud</p>
-     <p>dbvvghvhud</p>
-
             <HashRouter>
                 <Routes>
                     <Route exact path='/' element={<Home />}></Route>
@@ -32,49 +28,49 @@ const App = () => {
     )
 }
 
-  let [data, setData] = useState([]);
+let [data, setData] = useState([]);
 
-  let fetchData = () => {
+let fetchData = () => {
     fetch("http://localhost:10001/product")
-      .then((r) => r.json())
-      .then((server) => {
-        setData(server);
-      });
-  };
-
-  useEffect(() => {
-    fetchData();
-  }, [0]);
-  return (
-    <>
-      <globalContext.Provider value={data}>
-        <HashRouter>
-          <Routes>
-            <Route exact path="/" element={<Home />}></Route>
-            <Route exact path="/profile" element={<Profilepage />}></Route>
-            <Route
-              exact
-              path="/product-details"
-              element={<ProductDetails />}
-            ></Route>
-            <Route
-              exact
-              path="/seprate-category"
-              element={<SeprateCategoryPage />}
-            ></Route>
-            <Route
-              exact
-              path="/seprate-category"
-              element={<SeprateCategoryPage />}
-            ></Route>
-            <Route exact path="/cart" element={<Cart />}></Route>
-            <Route exact path="/payment" element={<Paymentpage />}></Route>
-          </Routes>
-        </HashRouter>
-      </globalContext.Provider>
-    </>
-  );
+        .then((r) => r.json())
+        .then((server) => {
+            setData(server);
+        });
 };
+
+useEffect(() => {
+    fetchData();
+}, [0]);
+return (
+    <>
+        <globalContext.Provider value={data}>
+            <HashRouter>
+                <Routes>
+                    <Route exact path="/" element={<Home />}></Route>
+                    <Route exact path="/profile" element={<Profilepage />}></Route>
+                    <Route
+                        exact
+                        path="/product-details"
+                        element={<ProductDetails />}
+                    ></Route>
+                    <Route
+                        exact
+                        path="/seprate-category"
+                        element={<SeprateCategoryPage />}
+                    ></Route>
+                    <Route
+                        exact
+                        path="/seprate-category"
+                        element={<SeprateCategoryPage />}
+                    ></Route>
+                    <Route exact path="/cart" element={<Cart />}></Route>
+                    <Route exact path="/payment" element={<Paymentpage />}></Route>
+                </Routes>
+            </HashRouter>
+        </globalContext.Provider>
+    </>
+);
+
 
 
 export default App;
